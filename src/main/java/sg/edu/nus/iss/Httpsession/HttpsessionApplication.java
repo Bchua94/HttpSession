c6e765ca609c03f73fc4ca4f520dc0d03faf857e
Bryan Chua <Bryanchua.1994@gmail.com>
2023-05-31 14:13:43 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/Httpsession/HttpsessionApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.Httpsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsessionApplication.class, args);
+	}
+
+}
